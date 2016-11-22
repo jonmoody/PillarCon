cfb258ae876a4f364302960602ff785367e4ddd9
Jonathan Moody <jonmoody12@gmail.com>
2016-11-21 23:09:12 -0500

    Added more reset logic - clearing memory and waiting for vertical blank

--- pillarcon.asm
+++ pillarcon.asm
@@ -11,6 +11,39 @@
 RESET:
   SEI
   CLD
+  LDX #$40
+  STX $4017    ; disable APU frame IRQ
+  LDX #$FF
+  TXS
+  INX
+  STX $2000    ; disable NMI
+  STX $2001    ; disable rendering
+  STX $4010    ; disable DMC IRQs
+
+VBlankWait1:
+  BIT $2002
+  BPL VBlankWait1
+
+ClearMemory:
+  LDA #$00
+  STA $0000, x
+  STA $0100, x
+  STA $0200, x
+  STA $0400, x
+  STA $0500, x
+  STA $0600, x
+  STA $0700, x
+  LDA #$FE
+  STA $0300, x
+  INX
+  BNE ClearMemory
+
+VBlankWait2:
+  BIT $2002
+  BPL VBlankWait2
+
+InfiniteLoop:
+  JMP InfiniteLoop
 
 NMI:
   RTI
