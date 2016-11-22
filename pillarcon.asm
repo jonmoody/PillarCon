eae3bc7496afece89ca242d325a04267239f29c0
Jonathan Moody <jonmoody12@gmail.com>
2016-11-21 21:56:55 -0500

    Added NMI and reset vectors

--- pillarcon.asm
+++ pillarcon.asm
@@ -3,17 +3,27 @@
   .inesmap 0
   .inesmir 1
 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
   .bank 0
   .org $C000
 
+RESET:
+  SEI
+  CLD
 
+NMI:
+  RTI
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
   .bank 1
-  .org $E000
+  .org $FFFA
+  .dw NMI
+  .dw RESET
+  .dw 0
 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
   .bank 2
   .org $0000
