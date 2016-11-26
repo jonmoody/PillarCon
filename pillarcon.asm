8ae5dc6d1742e67cc8d346b7c30a36c5fef3dd60
Jonathan Moody <jonmoody12@gmail.com>
2016-11-26 14:06:41 -0500

    Loading attributes and preventing the sprite from going off the side of the screen

--- pillarcon.asm
+++ pillarcon.asm
@@ -98,6 +98,20 @@ LoadBackgroundLoop:
   CPX #$04
   BNE LoadBackgroundLoop
 
+LoadAttribute:
+  LDA $2002
+  LDA #$23
+  STA $2006
+  LDA #$C0
+  STA $2006
+  LDX #$00
+LoadAttributeLoop:
+  LDA attribute, x
+  STA $2007
+  INX
+  CPX #$40
+  BNE LoadAttributeLoop
+
 
   LDA #%10000000    ; Enable NMI, sprites and background on table 0
   STA $2000
@@ -133,6 +147,11 @@ ReadLeft:
   BEQ ReadLeftDone
 
   LDA $0203       ; Sprite X Position
+
+  TAX
+  CPX #$00
+  BEQ ReadLeftDone
+
   SEC
   SBC #$01
   STA $0203
@@ -144,6 +163,11 @@ ReadRight:
   BEQ ReadRightDone
 
   LDA $0203       ; Sprite X position
+
+  TAX
+  CPX #$F9
+  BEQ ReadRightDone
+
   CLC
   ADC #$01
   STA $0203
@@ -171,7 +195,7 @@ palette:
   .db $0F,$16,$15,$14,  $0F,$16,$15,$14,  $0F,$16,$15,$14,  $0F,$16,$15,$14
 
 sprites:
-  .db $C0, $07, $00, $20
+  .db $C0, $07, $00, $20  ; Up Arrow
 
 background:
   .db $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
