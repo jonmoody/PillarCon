c34e8406461322140ca3b8af0fb42b24b9c869e2
Jonathan Moody <jonmoody12@gmail.com>
2016-12-19 23:27:51 -0500

    Added pointers for player sprite data

--- pillarcon.asm
+++ pillarcon.asm
@@ -26,6 +26,51 @@ projectileTile = $0211
 projectileAttr = $0212
 projectileX = $0213
 
+playerSprite1Y = $0224
+playerSprite1Tile = $0225
+playerSprite1Attr = $0226
+playerSprite1X = $0227
+
+playerSprite2Y = $0228
+playerSprite2Tile = $0229
+playerSprite2Attr = $022A
+playerSprite2X = $022B
+
+playerSprite3Y = $022C
+playerSprite3Tile = $022D
+playerSprite3Attr = $022E
+playerSprite3X = $022F
+
+playerSprite4Y = $0230
+playerSprite4Tile = $0231
+playerSprite4Attr = $0232
+playerSprite4X = $0233
+
+playerSprite5Y = $0234
+playerSprite5Tile = $0235
+playerSprite5Attr = $0236
+playerSprite5X = $0237
+
+playerSprite6Y = $0238
+playerSprite6Tile = $0239
+playerSprite6Attr = $023A
+playerSprite6X = $023B
+
+playerSprite7Y = $023C
+playerSprite7Tile = $023D
+playerSprite7Attr = $023E
+playerSprite7X = $023F
+
+playerSprite8Y = $0240
+playerSprite8Tile = $0241
+playerSprite8Attr = $0242
+playerSprite8X = $0243
+
+playerSprite9Y = $0244
+playerSprite9Tile = $0245
+playerSprite9Attr = $0246
+playerSprite9X = $0247
+
   .bank 0
   .org $C000
 
