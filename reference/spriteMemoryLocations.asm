1d97cbd5d8ce172cb028a599adb017b606d834f1
Jonathan Moody <jonmoody12@gmail.com>
2016-12-30 22:52:19 -0500

    Extracted memory location variables to a separate file

---
+++ reference/spriteMemoryLocations.asm
@@ -0,0 +1,69 @@
+projectileY = $0210
+projectileX = $0213
+
+enemyProjectileY = $0214
+enemyProjectileX = $0217
+
+playerSprite1Y = $0224
+playerSprite1Tile = $0225
+playerSprite1Attr = $0226
+playerSprite1X = $0227
+
+playerSprite2Y = $0228
+playerSprite2Tile = $0229
+playerSprite2Attr = $022A
+playerSprite2X = $022B
+
+playerSprite3Y = $022C
+playerSprite3Tile = $022D
+playerSprite3Attr = $022E
+playerSprite3X = $022F
+
+playerSprite4Y = $0230
+playerSprite4Tile = $0231
+playerSprite4Attr = $0232
+playerSprite4X = $0233
+
+playerSprite5Y = $0234
+playerSprite5Tile = $0235
+playerSprite5Attr = $0236
+playerSprite5X = $0237
+
+playerSprite6Y = $0238
+playerSprite6Tile = $0239
+playerSprite6Attr = $023A
+playerSprite6X = $023B
+
+playerSprite7Y = $023C
+playerSprite7Tile = $023D
+playerSprite7Attr = $023E
+playerSprite7X = $023F
+
+playerSprite8Y = $0240
+playerSprite8Tile = $0241
+playerSprite8Attr = $0242
+playerSprite8X = $0243
+
+playerSprite9Y = $0244
+playerSprite9Tile = $0245
+playerSprite9Attr = $0246
+playerSprite9X = $0247
+
+enemySprite1Y = $0248
+enemySprite1X = $024B
+enemySprite2Y = $024C
+enemySprite2X = $024F
+enemySprite3Y = $0250
+enemySprite3X = $0253
+enemySprite4Y = $0254
+enemySprite4X = $0257
+enemySprite5Y = $0258
+enemySprite5X = $025A
+enemySprite6Y = $025B
+enemySprite6X = $025F
+enemySprite7Y = $0260
+enemySprite7X = $0263
+enemySprite8Y = $0264
+enemySprite8X = $0267
+enemySprite9Y = $0268
+enemySprite9X = $026B
