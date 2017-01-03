cbf4a7d846cf05fcb4826c82a4bb84f2149a876d
Jonathan Moody <jonmoody12@gmail.com>
2017-01-02 22:58:40 -0500

    Vastly reduced the size of the dialog text - implemented newline functionality

--- pillarcon.asm
+++ pillarcon.asm
@@ -249,11 +249,145 @@ LoadBackgroundLoop:
   BNE LoadBackgroundLoop
   RTS
 
+NextLineTop:
+  CPY #$10
+  BNE TopLine3
+
+  LDA $2002
+  LDA #$20
+  STA $2006
+  LDA #$AC
+  STA $2006
+  JMP EndNextLineTop
+
+TopLine3:
+  CPY #$20
+  BNE TopLine4
+
+  LDA $2002
+  LDA #$20
+  STA $2006
+  LDA #$CC
+  STA $2006
+  JMP EndNextLineTop
+
+TopLine4:
+  CPY #$30
+  BNE TopLine5
+
+  LDA $2002
+  LDA #$20
+  STA $2006
+  LDA #$EC
+  STA $2006
+  JMP EndNextLineTop
+
+TopLine5:
+  CPY #$40
+  BNE TopLine6
+
+  LDA $2002
+  LDA #$21
+  STA $2006
+  LDA #$0C
+  STA $2006
+  JMP EndNextLineTop
+
+TopLine6:
+  CPY #$50
+  BNE TopLine7
+
+  LDA $2002
+  LDA #$21
+  STA $2006
+  LDA #$2C
+  STA $2006
+  JMP EndNextLineTop
+
+TopLine7:
+  CPY #$60
+  BNE EndNextLineTop
+
+  LDA $2002
+  LDA #$21
+  STA $2006
+  LDA #$4C
+  STA $2006
+EndNextLineTop:
+  RTS
+
+NextLineBottom:
+  CPY #$10
+  BNE BottomLine3
+
+  LDA $2002
+  LDA #$22
+  STA $2006
+  LDA #$8C
+  STA $2006
+  JMP EndNextLineBottom
+
+BottomLine3:
+  CPY #$20
+  BNE BottomLine4
+
+  LDA $2002
+  LDA #$22
+  STA $2006
+  LDA #$AC
+  STA $2006
+  JMP EndNextLineBottom
+
+BottomLine4:
+  CPY #$30
+  BNE BottomLine5
+
+  LDA $2002
+  LDA #$22
+  STA $2006
+  LDA #$CC
+  STA $2006
+  JMP EndNextLineBottom
+
+BottomLine5:
+  CPY #$40
+  BNE BottomLine6
+
+  LDA $2002
+  LDA #$22
+  STA $2006
+  LDA #$EC
+  STA $2006
+  JMP EndNextLineBottom
+
+BottomLine6:
+  CPY #$50
+  BNE BottomLine7
+
+  LDA $2002
+  LDA #$23
+  STA $2006
+  LDA #$0C
+  STA $2006
+  JMP EndNextLineBottom
+
+BottomLine7:
+  CPY #$60
+  BNE EndNextLineBottom
+
+  LDA $2002
+  LDA #$23
+  STA $2006
+  LDA #$2C
+  STA $2006
+EndNextLineBottom:
+  RTS
+
 LoadTopDialog:
   LDA $2002
   LDA #$20
   STA $2006
-  LDA #$80
+  LDA #$8C
   STA $2006
 
   LDY #$00
@@ -262,7 +396,9 @@ LoadTopDialogLoop:
   STA $2007
 
   INY
-  CPY #$E0
+  JSR NextLineTop
+
+  CPY #$70
   BNE LoadTopDialogLoop
   RTS
 
@@ -270,7 +406,7 @@ LoadBottomDialog:
   LDA $2002
   LDA #$22
   STA $2006
-  LDA #$60
+  LDA #$6C
   STA $2006
 
   LDY #$00
@@ -279,7 +415,9 @@ LoadBottomDialogLoop:
   STA $2007
 
   INY
-  CPY #$E0
+  JSR NextLineBottom
+  
+  CPY #$70
   BNE LoadBottomDialogLoop
   RTS
 
