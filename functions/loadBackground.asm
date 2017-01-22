9817312a3090c9df6cdfa9fde0c727063bebaa68
Jonathan Moody <jonmoody12@gmail.com>
2017-01-22 14:43:09 -0500

    Added method to clear the background

--- functions/loadBackground.asm
+++ functions/loadBackground.asm
@@ -20,3 +20,25 @@ LoadBackground:
   CPX #$04
   BNE .Loop
   RTS
+
+ClearBackground:
+  LDA $2002
+  LDA #$20
+  STA $2006
+  LDA #$00
+  STA $2006
+
+  LDX #$00
+  LDY #$00
+.Loop:
+  LDA #$00
+  STA $2007
+
+  INY
+  CPY #$00
+  BNE .Loop
+
+  INX
+  CPX #$04
+  BNE .Loop
+  RTS
