c56b7ac996fd78039f6279bbf05b3ab9357b4d03
Jonathan Moody <jonmoody12@gmail.com>
2017-02-13 22:17:32 -0500

    New enemy design

--- graphics/palette.asm
+++ graphics/palette.asm
@@ -1,2 +1,2 @@
   .db $0F,$21,$16,$20,  $0F,$27,$27,$17,  $0F,$19,$16,$20,  $29,$2A,$18,$0F ; Background: Text/Hearts, Forge, Unused, Ground
-  .db $0F,$21,$15,$14,  $0F,$37,$30,$0F,  $0F,$16,$10,$0F,  $0F,$0F,$37,$11 ; Sprites: Unused, Traveler, Enemy, Player
+  .db $0F,$21,$15,$14,  $0F,$37,$30,$0F,  $0F,$15,$10,$0F,  $0F,$0F,$37,$11 ; Sprites: Unused, Traveler, Enemy, Player
